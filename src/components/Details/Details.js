a809e106ac48be6b5afc4538ecab75fabf287c76
distoneman <distoneman@gmail.com>
2019-01-18 14:34:44 -0700

    updated

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './details.scss'
 import axios from 'axios';
+import Button from '@material-ui/core/Button';
 
 export default class Details extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            product: [],
+            product: {},
             quantity: 1,
         }
     }
@@ -20,19 +21,28 @@ export default class Details extends Component {
 
     updateQuantity(operator) {
         let qty = this.state.quantity;
-        if(operator === 'minus' & this.state.quantity > 1){
-            qty = qty -1;
-            this.setState({quantity: qty})
-        } else if(operator === 'add') {
+        if (operator === 'minus' & this.state.quantity > 1) {
+            qty = qty - 1;
+            this.setState({ quantity: qty })
+        } else if (operator === 'add') {
             qty++
-            this.setState({quantity: qty})
+            this.setState({ quantity: qty })
         }
     }
 
+    addToCart() {
+        console.log('Add to Cart Hit');
+        let quantity = this.state.quantity;
+        let prod_id = this.state.product.prod_id;
+        console.log(quantity);
+        console.log(prod_id);
+    }
+
+
     render() {
         const { prod_name, prod_desc, prod_price, prod_size, prod_image } = this.state.product;
-        if(prod_price)var price = prod_price.toLocaleString('us-US', {style: 'currency', currency: 'USD'})
-
+        if (prod_price) var price = prod_price.toLocaleString('us-US', { style: 'currency', currency: 'USD' })
+        console.log(this.state)
         return (
             <div>
                 <div className='product-container'>
@@ -54,18 +64,23 @@ export default class Details extends Component {
                             <span className='qty-span'>
                                 <button onClick={(e) => this.updateQuantity('minus')}
                                     className='btn-quantity'>-</button>
-                                <input className='input-quantity' type="text" value={this.state.quantity}  readonly/>
+                                <input className='input-quantity' type="text" value={this.state.quantity} readOnly />
                                 <button onClick={(e) => this.updateQuantity('add')}
                                     className='btn-quantity'>+</button>
                             </span>
                         </p>
+                        <p>
+                            <Button onClick={() => this.addToCart()} variant="outlined" size="small" className='btn-login' >
+                                Add to Cart
+                            </Button>
+                        </p>
                     </div>
                 </div>
                 <div className="prod-desc">
-                        <h3>
-                            Product Description:<br/>
-                        </h3>
-                        {prod_desc}
+                    <h3>
+                        Product Description:<br />
+                    </h3>
+                    {prod_desc}
                 </div>
             </div>
         )
